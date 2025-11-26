bf1028065242d16e1902f688b8da47d96b8d2603
devnawa <148815756+devnawa@users.noreply.github.com>
2025-11-27 00:05:53 +0300

    fix: import intl from correct package

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { PortableTextBlock } from "next-sanity";
-import { DateTimeFormatOptions } from "intl";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -17,6 +16,6 @@ export function calculateReadingTime(content: PortableTextBlock[]) {
   };
 }
 
-export function formatDate(date: string, format: DateTimeFormatOptions = { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" }) {
+export function formatDate(date: string, format: Intl.DateTimeFormatOptions = { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" }) {
   return new Date(date).toLocaleDateString("tr-TR", format);
 }
